297667ad2d56a3e95e67c1272ff606013447383e
ethanzohar <ethanzohar9@gmail.com>
2020-10-23 18:24:10 -0400

    back to port 80

--- proxy-settings.js
+++ proxy-settings.js
@@ -12,7 +12,7 @@ module.exports = {
   // the ports
   ports: {
     // where you want to run the 'joint' proxied server
-    main: 3000,
+    main: 80,
     // where you want to run the react-dev-server
     react: 8081,
     // where you serve your api (make sure to serve it on that port)
