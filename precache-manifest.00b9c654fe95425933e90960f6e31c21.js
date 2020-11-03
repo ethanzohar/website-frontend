87658b3ae3f38ddae0d44b26050be69479028b13
ethanzohar <ethanzohar9@gmail.com>
2020-11-02 19:18:54 -0500

    Updates

---
+++ precache-manifest.00b9c654fe95425933e90960f6e31c21.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed78891a131c2927435043c1a65fbef",
+    "url": "/website-frontend/index.html"
+  },
+  {
+    "revision": "be8d37c20737f261544f",
+    "url": "/website-frontend/static/css/main.a74f4f76.chunk.css"
+  },
+  {
+    "revision": "f4e8a32bfe4c32ddb872",
+    "url": "/website-frontend/static/js/2.6179b645.chunk.js"
+  },
+  {
+    "revision": "25cb5ecac244be137e0add3cc7b46de0",
+    "url": "/website-frontend/static/js/2.6179b645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8d37c20737f261544f",
+    "url": "/website-frontend/static/js/main.050b59f4.chunk.js"
+  },
+  {
+    "revision": "7fbfc58bb90734b8eb81",
+    "url": "/website-frontend/static/js/runtime-main.8140e7cc.js"
+  },
+  {
+    "revision": "e19c20e966bde501f94e41cd0322dbe8",
+    "url": "/website-frontend/static/media/Gotham.e19c20e9.otf"
+  },
+  {
+    "revision": "fcca78a099cd0eedd161282ad29e1a0d",
+    "url": "/website-frontend/static/media/collage2.fcca78a0.png"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/website-frontend/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "6105f9af4d7d433aca76071a8bf3a9c5",
+    "url": "/website-frontend/static/media/headerImage1.6105f9af.jpg"
+  },
+  {
+    "revision": "54b42e4727ab12b63e39dbb12cddacef",
+    "url": "/website-frontend/static/media/headerImage2.54b42e47.jpg"
+  },
+  {
+    "revision": "c8f20fa65f9252a2c2cfde5a045fd263",
+    "url": "/website-frontend/static/media/headerImage3.c8f20fa6.jpg"
+  },
+  {
+    "revision": "4d2b942a5a1dabbfd4fb8cc3b2be0cf0",
+    "url": "/website-frontend/static/media/headshot.4d2b942a.jpg"
+  },
+  {
+    "revision": "833a55e8dddcab1fce44a8544233e1e5",
+    "url": "/website-frontend/static/media/instagram.833a55e8.webp"
+  },
+  {
+    "revision": "3f60edfd3589f7ad589cd03676982f12",
+    "url": "/website-frontend/static/media/linkedin.3f60edfd.png"
+  },
+  {
+    "revision": "d0519693bca8bcdc114ee68da581c68e",
+    "url": "/website-frontend/static/media/resume.d0519693.pdf"
+  },
+  {
+    "revision": "bd1fa81f07d4d52ab52bc407f5843acc",
+    "url": "/website-frontend/static/media/screenshareStream.bd1fa81f.png"
+  },
+  {
+    "revision": "b88676d1d5505bd5372cee340d93f538",
+    "url": "/website-frontend/static/media/screenshareWatch.b88676d1.png"
+  },
+  {
+    "revision": "8f474fe177c8c9488b01aedbd346542f",
+    "url": "/website-frontend/static/media/transcript.8f474fe1.pdf"
+  },
+  {
+    "revision": "4cb898aea458ccfe27cbf54a757f463a",
+    "url": "/website-frontend/static/media/unknown_album.4cb898ae.jpg"
+  },
+  {
+    "revision": "edadad53dd686888b967b814200f8ca3",
+    "url": "/website-frontend/static/media/viberListenView.edadad53.png"
+  },
+  {
+    "revision": "1548cef721f5fe23a68279f4c2c84de9",
+    "url": "/website-frontend/static/media/viberStreamView.1548cef7.png"
+  },
+  {
+    "revision": "24aad3e40cf18af66a91baacdda81574",
+    "url": "/website-frontend/static/media/whiteboards.24aad3e4.png"
+  }
+]);
